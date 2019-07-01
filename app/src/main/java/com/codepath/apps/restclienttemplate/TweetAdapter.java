319059c371b4e743c6d96188069e33358daf835d
jkallini <jkallini@princeton.edu>
2019-06-30 18:19:39 -0700

    Created Java models, custom row layout, and Tweet Adapter

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -0,0 +1,70 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.util.List;
+
+public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder>{
+
+    private List<Tweet> mTweets; // list of tweets
+
+    // constructor takes a tweet array
+    public TweetAdapter(List<Tweet> tweets) {
+        mTweets = tweets;
+    }
+
+    // for each row, inflate the layout and cache references into ViewHolder
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View tweetView = inflater.inflate(R.layout.item_tweet, parent, false);
+        ViewHolder viewHolder = new ViewHolder(tweetView);
+        return viewHolder;
+    }
+
+    // bind the values based on the position of the element
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // get the data according to the position
+        Tweet tweet = mTweets.get(position);
+
+        // populate the views according to this data
+        holder.tvUsername.setText(tweet.user.name);
+        holder.tvBody.setText(tweet.body);
+
+        // TODO - populate the profile image using glide
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTweets.size();
+    }
+
+    // custom Viewholder class
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        // references of views from the itemView passed in constructor
+        public ImageView ivProfileImage;
+        public TextView tvUsername;
+        public TextView tvBody;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            // lookup views
+            ivProfileImage = (ImageView) itemView.findViewById(R.id.ivProfileImage);
+            tvUsername = (TextView) itemView.findViewById(R.id.tvUsername);
+            tvBody = (TextView) itemView.findViewById(R.id.tvBody);
+        }
+    }
+}
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,24 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Tweet {
+
+    // list out the attributes
+    public String body;
+    public long uid; // database ID for the tweet
+    public String createdAt;
+    public User user;
+
+    // deserialize the JSON
+    public static Tweet fromJSON(JSONObject jsonObject) throws JSONException {
+        Tweet tweet = new Tweet();
+
+        tweet.body = jsonObject.getString("text");
+        tweet.uid = jsonObject.getLong("id");
+        tweet.createdAt = jsonObject.getString("created_at");
+        tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
+        return tweet;
+    }
+}
