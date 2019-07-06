9bde7a6624a428e8849061676616b74b636700f1
jkallini <jkallini@princeton.edu>
2019-07-05 17:58:51 -0700

    Added gifs

--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -26,8 +26,10 @@ public class TwitterClient extends OAuthBaseClient {
 	public static final String REST_URL = "https://api.twitter.com/1.1"; // Change this, base API URL
 	//public static final String REST_CONSUMER_KEY = "4KxocRp2Wh8RZ9cy1KJEjxGVy";       // Change this
 	//public static final String REST_CONSUMER_SECRET = "EeyJ4vEZN3al7c0C13bMwAY3pGc2RASrampYtvJvnX1kLDHKJf"; // Change this
-	public static final String REST_CONSUMER_KEY = "ABaCtvkJthocDnKfbw8REoOW4";
-	public static final String REST_CONSUMER_SECRET = "uVntI345xJ0vTSyExmbTqUvLd2vo8SVznfiLo3tewrsjUodeTa";
+	//public static final String REST_CONSUMER_KEY = "ABaCtvkJthocDnKfbw8REoOW4";
+	//public static final String REST_CONSUMER_SECRET = "uVntI345xJ0vTSyExmbTqUvLd2vo8SVznfiLo3tewrsjUodeTa";
+	public static final String REST_CONSUMER_KEY = "rllBCzpDlSKxcP4KLfYaHIqtV";
+	public static final String REST_CONSUMER_SECRET = "kybWvvX9paFvOiUjNG7S8Mgexi2XVMp37eisBRfUfUtkXFpT3z";
 
 	// Landing page to indicate the OAuth flow worked in case Chrome for Android 25+ blocks navigation back to the app.
 	public static final String FALLBACK_URL = "https://codepath.github.io/android-rest-client-template/success.html";
